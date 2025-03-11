ed09119e6041e61e29b3fbcde261c474d628acf8
CASHIKPOOJARY <cashikpoojary@gmail.com>
2025-03-11 16:10:32 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "This is C Ashik Poojary , SRN : PES1UG22CS150\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
