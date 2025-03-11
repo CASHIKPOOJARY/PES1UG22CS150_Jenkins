5fd20877398cf55fe925d151fad329a10804787a
CASHIKPOOJARY <cashikpoojary@gmail.com>
2025-03-11 16:53:14 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "Hello from C++ program!, C ASHIK POOJARY , PES1UG22CS150" << endl;
+    cout << "Hello from C++ program!, C ASHIK POOJARY , PES1UG22CS150" << endl
     return 0;
 }
